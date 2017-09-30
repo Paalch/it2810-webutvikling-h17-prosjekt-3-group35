6c6a25aaa4917971aa2a2fa61f9347a6a3392144
Kristiane Westgård <kristianeaw@gmail.com>
2017-09-30 22:18:50 +0200

    Todo done (almost)

--- react-app/src/containers/todo/TodoList.js
+++ react-app/src/containers/todo/TodoList.js
@@ -1,49 +1,68 @@
-import React, { Component } from 'react';
-import { Col, Row, FormControl, Button, Checkbox } from 'react-bootstrap';
+import React, {Component} from 'react';
+import {Col, Row, FormControl, Button} from 'react-bootstrap';
+import TodoElement from "./TodoElement";
 
 class TodoList extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+      text: ''
+    }
+    this.updateTextField = this.updateTextField.bind(this);
+    this.saveTodo = this.saveTodo.bind(this);
+    this.removeElement = this.removeElement.bind(this);
+  }
+
+  updateTextField(e) {
+    this.setState({text: e.target.value});
+  }
+
+  removeElement(todo) {
+    this.setState({
+      todos: this.state.todos.filter((t) =>
+        t !== todo
+      )
+    });
+  }
+
   saveTodo() {
-    const todoValue = document.getElementById('todo-form-input').value;
+    const todoValue = this.state.text;
     if (todoValue.length === 0) {
       return;
     }
-
-    const todoDiv = document.createElement('div');
-    todoDiv.className = 'todo-element';
-    todoDiv.id = todoValue;
-
-    const todo = document.createElement('input');
-    todo.type = 'checkbox';
-    todo.value = todoValue;
-
-    const label = document.createElement('label');
-    label.appendChild(document.createTextNode(todoValue));
-
-    todoDiv.appendChild(todo);
-    todoDiv.appendChild(label);
-
-    document.getElementById('todos').appendChild(todoDiv);
-    document.getElementById('todo-form-input').value = '';
+    this.setState({todos: this.state.todos.concat(todoValue), text: ''});
   };
 
   render() {
+    const content = this.state.todos.map((todo, i) =>
+      <TodoElement text={todo} key={i} removeElement={this.removeElement}/>
+    );
+
     return (
       <div>
         <Row>
           <Col md={10} xs={9}>
             <FormControl
-              id = 'todo-form-input'
-              type='text'
-              placeholder='Todo...'
+              id={'todo-form-input'}
+              type={'text'}
+              placeholder={'Todo...'}
+              value={this.state.text}
+              onChange={this.updateTextField}
             />
           </Col>
           <Col md={2} xs={3}>
-            <Button className={'todo-add-button'} bsStyle={'primary'} onClick={this.saveTodo}>+</Button>
+            <Button
+              className={'todo-add-button'}
+              bsStyle={'primary'}
+              onClick={this.saveTodo}>
+              +
+            </Button>
           </Col>
         </Row>
         <div id={'todos'}>
-
+          {content}
         </div>
       </div>
     )

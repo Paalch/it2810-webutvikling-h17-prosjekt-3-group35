3b4c5a6b3182c384a1c12b52bb056f75ec774379
didrikpa <didrikpa@gmail.com>
2017-10-02 15:44:31 +0200

    Added date and timepicker. Not functioning quite right yet.

--- react-app/src/containers/schedule/Schedule.js
+++ react-app/src/containers/schedule/Schedule.js
@@ -1,20 +1,46 @@
 import React from 'react';
-import { Row, Col } from 'react-bootstrap'
+import {Row, Col} from 'react-bootstrap'
+import DatePicker from 'react-datepicker'
+import Timepicker from 'react-timepicker'
+import moment from 'moment'
+
 import './schedule.css';
+import 'react-datepicker/dist/react-datepicker-cssmodules.css';
+
 
 class Schedule extends React.Component {
-  render () {
-    return (
-      <div className={'schedule-container box'}>
-        <Row className={'box-header'}>
-          <h1>Schedule</h1>
-        </Row>
-        <Row className={'box-body'}>
-          <p>Lorem ipsum</p>
-        </Row>
-      </div>
-    )
-  }
+
+    constructor() {
+        super();
+        this.state = {
+            startDate: moment()
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(date) {
+        this.setState({
+            startDate: date
+        });
+    }
+    render() {
+        return (
+            <div className={'schedule-container box'}>
+                <Row className={'box-header'}>
+                    <h1>Schedule</h1>
+                </Row>
+                <Row className={'box-body'}>
+                    <p>
+                        <DatePicker
+                        selected={this.state.startDate}
+                        onChange={this.handleChange}
+                        />
+                        <Timepicker/>
+                    </p>
+                </Row>
+            </div>
+        )
+    }
 }
 
 export default Schedule;

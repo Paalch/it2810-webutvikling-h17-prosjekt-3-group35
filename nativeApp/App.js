d61981ef4a6aa693e4f322efe5e3d71ecd66b428
SkylineTardis <martiagr@stud.ntnu.no>
2017-10-03 11:36:19 +0200

    main view design finished

--- nativeApp/App.js
+++ nativeApp/App.js
@@ -4,11 +4,26 @@ import {StyleSheet, View, Text} from 'react-native';
 export default class App extends React.Component {
   render() {
     return (
-        <View>
+        <View style={styles.content}>
           <View style={styles.headerContainer}>
             <Text style={styles.text}>SaveMyLife.com</Text>
           </View>
           <View style={styles.back}>
+              <View style={styles.scheduleContainer}>
+                  <View style={styles.scheduleBoxHeader}>
+                      <Text style={styles.innerText}>Schedule</Text>
+                  </View>
+              </View>
+              <View style={styles.todoContainer}>
+                  <View style={styles.todoBoxHeader}>
+                      <Text style={styles.innerText}>2do</Text>
+                  </View>
+              </View>
+              <View style={styles.notesContainer}>
+                  <View style={styles.notesBoxHeader}>
+                      <Text style={styles.innerText}>Notes</Text>
+                  </View>
+              </View>
           </View>
         </View>
     );
@@ -16,9 +31,14 @@ export default class App extends React.Component {
 }
 
 const styles = StyleSheet.create({
+    content: {
+        flex: 1,
+    },
+
     headerContainer: {
         backgroundColor: '#212a34',
-        paddingTop: 40,
+        elevation: 6,
+        paddingTop: 30,
         paddingBottom: 10,
         shadowColor: 'rgba(0,0,0,0.3)',
         shadowOpacity: 3,
@@ -30,11 +50,73 @@ const styles = StyleSheet.create({
 
     text: {
         color: '#fff',
-        textAlign: 'center'
+        textAlign: 'center',
+        fontFamily: 'sans-serif',
+        fontWeight: 'bold'
     },
 
     back: {
         backgroundColor: "#666",
-        height: '100%'
+        flex: 1
+    },
+
+    scheduleContainer: {
+        backgroundColor: "#cfc",
+        marginLeft: '3%',
+        marginRight: '3%',
+        marginTop: '3%',
+        display: 'flex',
+        elevation: 6,
+        shadowColor: 'rgba(33,33,33,.7)',
+        shadowOpacity: 7,
+        shadowOffset: {
+            height: 5,
+            width: 5
+        },
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    todoContainer: {
+        backgroundColor: "#ccf",
+        marginLeft: '3%',
+        marginRight: '3%',
+        marginTop: '3%',
+        display: 'flex',
+        elevation: 6,
+        shadowColor: 'rgba(33,33,33,.7)',
+        shadowOpacity: 7,
+        shadowOffset: {
+            height: 5,
+            width: 5
+        },
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    notesContainer: {
+        backgroundColor: "#FFFFA5",
+        marginLeft: '3%',
+        marginRight: '3%',
+        marginTop: '3%',
+        marginBottom: '3%',
+        display: 'flex',
+        elevation: 6,
+        shadowColor: 'rgba(33,33,33,.7)',
+        shadowOpacity: 7,
+        shadowOffset: {
+            height: 5,
+            width: 5
+        },
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    innerText: {
+        fontSize: 40,
+        color: '#212a34'
     }
 });

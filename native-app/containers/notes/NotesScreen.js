fc85b483e3fcf3d7e5ee325b1a97f27031a1d591
SkylineTardis <martiagr@stud.ntnu.no>
2017-10-10 11:22:09 +0200

    added functionality to notes, does not work yet though

--- native-app/containers/notes/NotesScreen.js
+++ native-app/containers/notes/NotesScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default class NotesScreen extends React.Component {
     static navigationOptions = {
@@ -8,18 +8,59 @@ export default class NotesScreen extends React.Component {
         headerTitleStyle: { color: '#fff',},
         headerTintColor: '#fff'
     };
-    render() {
+
+    constructor() {
+        super();
+        this.state = {
+            notes: [],
+            currentKeyCounter: 0
+        };
+        this.addNewNote = this.addNewNote.bind(this);
+        this.deleteNote = this.deleteNote.bind(this);
+        this.updateTextField = this.updateTextField.bind(this);
+    }
+
+    addNewNote() {
+        this.setState({
+            notes: this.state.notes.concat({id: this.state.currentKeyCounter, text: ''}),
+            currentKeyCounter: this.state.currentKeyCounter + 1
+        });
+    }
+
+    deleteNote(index) {
+        const tempNotes = this.state.notes.filter(n => n.id !== index);
+        this.setState({notes: tempNotes,currentKeyCounter: this.state.currentKeyCounter});
+    }
+
+    updateTextField(index, text) {
+        const tempNotes = this.state.notes;
+        tempNotes.forEach((note) => {
+            if(note.id === index) {
+                note.text = text;
+            }
+        });
+        this.setState({notes: tempNotes, currentKeyCounter: this.state.currentKeyCounter})
+
+    }
+
+    render () {
+        const content = this.state.notes.map((note, i) =>
+            <NoteElement key={i} note={note} updateTextField={this.updateTextField} deleteNote={this.deleteNote}/>
+        );
+
         return (
             <View style={styles.container}>
-                <Text>Chat with Lucy</Text>
+                <div style={styles.notesBody}>
+                   { content }
+                </div>
+                <Button bsStyle={'warning'} onClick={this.addNewNote}>+ New note</Button>
             </View>
-        );
+        )
     }
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
         backgroundColor: "#666",
     },
 });

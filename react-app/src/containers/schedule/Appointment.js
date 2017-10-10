0abc139d1d76afe55bbeed4ab4c38601007b289e
didrikpa <didrikpa@gmail.com>
2017-10-10 17:43:20 +0200

    Appointment element is now functioning. Needs som styling.

--- react-app/src/containers/schedule/Appointment.js
+++ react-app/src/containers/schedule/Appointment.js
@@ -1,31 +1,48 @@
 import React, {Component} from 'react'
+import {Row, Col, Button} from 'react-bootstrap'
+import "./appointment.css"
 
-class Appointment extends Component{
 
-    constructor(props){
+class Appointment extends Component {
+
+    constructor(props) {
         super(props);
-        this.state ={
-
-        }
+        this.state = {
+            appointments: this.props.appointments
+        };
+        this.deleteAppointment = this.deleteAppointment.bind(this);
     }
 
-    render(){
-        return(
-          <div>
-              <Row>
-                  <Col>
-                      {props.date}
-                  </Col>
-                  <Col>
-                      {props.time}
-                  </Col>
-                  <Col>
-                      {props.details}
-                  </Col>
-              </Row>
-          </div>
+    deleteAppointment(){
+        let appointments = this.state.appointments;
+        for (let i = 0; i < appointments.length; i++){
+            console.log(appointments[i]);
+            if (appointments[i].includes(this.props.id)){
+                appointments.splice(i, 1);
+            }
+        }
+        this.setState({
+            appointments: appointments
+        });
+        this.props.receiveAppointment(this.state.appointments);
+    }
+
+
+    render() {
+        console.log("Appo " + this.state.appointments);
+        return (
+            <div className={'appointment-container'}>
+                <Row>
+                    <Col>
+                        <Button onClick={this.deleteAppointment}>X</Button>
+                    </Col>
+                    <Col>
+                        <label>{this.props.text}</label>
+                    </Col>
+                </Row>
+            </div>
         );
     }
 }
 
-export default Appointment
+export default Appointment;

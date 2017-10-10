7a9636d237927bb504596e0f009c8c30e9c34692
SkylineTardis <martiagr@stud.ntnu.no>
2017-10-10 13:37:16 +0200

    noteElement is finished

--- native-app/containers/notes/NoteElement.js
+++ native-app/containers/notes/NoteElement.js
@@ -5,19 +5,22 @@ export default class NotesElement extends React.Component {
     render () {
         return (
             <View style={styles.innerNotesContainer}>
-                <TouchableOpacity onPress={() => this.props.deleteNote(this.props.note.id)}>
-                    <Text>X</Text>
+                <TouchableOpacity style={styles.deleteButton} onPress={() => this.props.deleteNote(this.props.note.id)}>
+                    <Text style={styles.innerText}>X</Text>
                 </TouchableOpacity>
-                <View Style={styles.notes}>
-                    <View style={styles.boxBody}>
+                    <View>
                      <TextInput
+                         style={styles.noteField}
                          value={this.props.note.text}
                          onChange={(event) => this.props.updateTextField(this.props.note.id, event.target.value)}
                          id={this.props.note.id}
-                         maxRows={7}>
+                         multiline={true}
+                         numberOfLines={4}
+                         underlineColorAndroid='rgba(0,0,0,0)'
+                         placeholder={"Start typing here..."}
+                         placeholderTextColor={"#666"}>
                      </TextInput>
                     </View>
-                </View>
             </View>
         )
     }
@@ -35,5 +38,13 @@ const styles = StyleSheet.create({
             width: 6
         },
         marginBottom: "2%",
+    },
+
+    deleteButton: {
+        alignSelf: "flex-end",
+        alignItems: "flex-end",
+        paddingLeft: 10,
+        paddingRight: 10,
+        width: "10%",
     },
 });

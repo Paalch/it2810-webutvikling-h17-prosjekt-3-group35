dcaf249d82429fec81b353736f3d7d11e8e797d8
SkylineTardis <martiagr@stud.ntnu.no>
2017-10-11 15:30:11 +0200

    Todo is finally up and running

--- native-app/containers/todo/TodoElement.js
+++ native-app/containers/todo/TodoElement.js
@@ -1,28 +1,50 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
-import PropTypes from 'prop-types';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default class TodoElement extends React.Component {
     render() {
         return (
-            <View>
-                <input
-                    style={styles.todoElement}
-                    type={'checkbox'}
-                    onProgress={() => this.props.removeElement(this.props.text)}/>
-                <label>{this.props.text}</label>
+            <View key={this.props.keyval} style={styles.todo}>
+                <TouchableOpacity onPress={this.props.removeTodo} style={styles.checkBox}>
+                    <Text> </Text>
+                </TouchableOpacity>
+                <Text style={styles.todoTekst}>
+                    {this.props.val.text}
+                </Text>
             </View>
         );
     }
 }
 
-TodoElement.propTypes = {
-    text: PropTypes.string.isRequired
-};
-
 const styles = StyleSheet.create({
-    todoElement: {
-        fontWeight: "normal",
+    todo: {
+        padding: 20,
+        position: "relative",
+        paddingRight: 100,
+        borderBottomWidth: 2,
+        borderBottomColor: "#cbf",
+    },
+
+    todoTekst: {
+        color: "black",
+        paddingLeft: 30,
+    },
+
+    checkBox: {
+        backgroundColor: "#fff",
+        width: 25,
+        height: 25,
+        borderRadius: 50,
+        borderWidth: 1,
+        borderColor: "#ccccff",
+        position: "absolute",
+        top: 15,
+        left: 10,
+        elevation: 2,
+        alignItems: "center",
+        alignSelf: "flex-start",
+        justifyContent: "center",
+        borderStyle: "solid",
     },
 });
 
--- native-app/containers/todo/TodoScreen.js
+++ native-app/containers/todo/TodoScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import TodoList from './TodoList';
+import { StyleSheet, Text, View, ScrollView, TextInput, TouchableOpacity } from 'react-native';
+import TodoElement from "./TodoElement"
 
 export default class TodoScreen extends React.Component {
     static navigationOptions = {
@@ -9,14 +9,52 @@ export default class TodoScreen extends React.Component {
         headerTitleStyle: { color: '#fff',},
         headerTintColor: '#fff'
     };
+
+    state = {
+        todos: [],
+        text: ''
+    };
+
     render() {
+
+        let content = this.state.todos.map((todo, i) => {
+            return <TodoElement key={i} keyval={i} val={todo} removeTodo={() => this.removeElement(i)}/>
+        });
+
         return (
             <View style={styles.container}>
-                <View style={styles.todoBody}>
-                    <TodoList />
+                <View style={styles.textContainer}>
+
+                    <TextInput style={styles.textInput}
+                        placeholder="Tap here to write..."
+                        placeholderTextColor="#666"
+                        underlineColorAndroid="transparent"
+                        onChangeText={(text) => this.setState({text})}
+                        value={this.state.text}>
+                    </TextInput>
+
+                    <TouchableOpacity onPress={this.saveTodo.bind(this)} style={styles.addButton}>
+                        <Text style={styles.addButtonText}>+</Text>
+                    </TouchableOpacity>
                 </View>
+                <ScrollView style={styles.scrollview}>
+                    {content}
+                </ScrollView>
             </View>
         );
+    }
+
+    saveTodo() {
+        if (this.state.text ) {
+            this.state.todos.push({'text': this.state.text});
+            this.setState({todos: this.state.todos});
+            this.setState({text: ''});
+        }
+    };
+
+    removeElement(todo) {
+        this.state.todos.splice(todo, 1);
+        this.setState({todos: this.state.todos});
     }
 }
 
@@ -26,5 +64,42 @@ const styles = StyleSheet.create({
         backgroundColor: "#666",
         padding: 10,
     },
+
+    textContainer: {
+        backgroundColor: "#ccccff",
+        position: "absolute",
+        top: 10,
+        left: 10,
+        right: 10,
+        elevation: 2,
+    },
+
+    addButton: {
+        alignItems: "center",
+        alignSelf: "flex-end",
+        justifyContent: "center",
+        backgroundColor: "#cbf",
+        width: 90,
+        height: 70,
+        elevation: 8,
+        marginTop: -45,
+    },
+
+    addButtonText: {
+        fontSize: 24,
+        color: '#212a34',
+    },
+
+    scrollview: {
+        backgroundColor: "#ccccff",
+        marginTop: 70,
+        flex: 1
+    },
+
+    textInput: {
+        paddingLeft: 20,
+        paddingTop: 10
+    },
+
 });
 

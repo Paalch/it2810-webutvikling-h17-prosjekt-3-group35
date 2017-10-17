0d1264dccd090ee906b5f189f3b6cb157a1c34c1
Kristiane Westgård <kristianeaw@gmail.com>
2017-10-17 14:06:52 +0200

    Styled schedule, and some general things

--- react-app/src/containers/schedule/Appointment.js
+++ react-app/src/containers/schedule/Appointment.js
@@ -1,19 +1,30 @@
 import React, {Component} from 'react'
-import {Button} from 'react-bootstrap'
+import {Row, Col, Button, Panel} from 'react-bootstrap'
 import "./schedule.css"
 
 class Appointment extends Component {
   render() {
+    const header =
+      <div>
+        <Row>
+          <Col xs={8}>
+            <p>{this.props.time}</p>
+          </Col>
+          <Col xs={4}>
+            <Button
+              className={"delete-appointment"}
+              onClick={() => this.props.deleteAppointment(this.props.appointment.id)}
+              bsStyle={'link'}>
+              X
+            </Button>
+          </Col>
+        </Row>
+      </div>
     return (
-      <div className={'appointment-container'} style={{backgroundColor: this.props.color}}>
-        <label className={"appointment-content"}>{this.props.time}</label>
-        <label>{this.props.text}</label>
-        <Button
-          className={"delete-appointment"}
-          onClick={() => this.props.deleteAppointment(this.props.appointment.id)}
-          style={{backgroundColor: this.props.color}}>
-          X
-        </Button>
+      <div className={'appointment-container'}>
+        <Panel header={header} bsStyle={this.props.style}>
+          {this.props.text}
+        </Panel>
       </div>
     );
   }

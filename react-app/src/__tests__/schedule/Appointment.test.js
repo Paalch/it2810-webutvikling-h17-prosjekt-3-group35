0f5dba439a1117de70793d5f98dc5e1ea3f28e53
Paalch <pciverse@stud.ntnu.no>
2017-10-18 20:45:36 +0200

    Added remaining test files

--- react-app/src/__tests__/schedule/Appointment.test.js
+++ react-app/src/__tests__/schedule/Appointment.test.js
@@ -2,21 +2,19 @@ import React from 'react'
 import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-15'
 import renderer from 'react-test-renderer'
-import CreateAppointment from "../../containers/schedule/CreateAppointment";
+import Appointment from "../../containers/schedule/Appointment";
 
 
 Enzyme.configure({ adapter: new Adapter() })
 
 
-describe('Checks that the createNewAppointment component renders correctly', () => {
+describe('Checks that the schedule component renders correctly', () => {
 
-    it('Checks that newAppointment renders', () => {
+    it('Checks that Appointment renders', () => {
         const rendered = renderer.create(
-            <CreateAppointment />
+            <Appointment />
         );
         expect(rendered.toJSON()).toMatchSnapshot()
     });
-
-
 
 });
